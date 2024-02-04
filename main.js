ea22c3b396f338a401b2d3c3b28238d730f6fdb8
Nolan <mr.sirartemios@gmail.com>
2024-02-04 16:00:09 +0100

    V 1.0.0 - 07
    - Fixed autoupdate
    - Bugs fixes

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
+
 const { app, BrowserWindow, globalShortcut } = require('electron')
 const { autoUpdater, AppUpdater  } = require("electron-updater")
 
 autoUpdater.autoDownload = false;
 autoUpdater.autoInstallOnAppQuit = true;
 
-// if (app.isPackaged) {
-//   const server = 'https://your-deployment-url.com'
-//   const url = `${server}/update/${process.platform}/${app.getVersion()}`
+let win;
 
-//   autoUpdater.setFeedURL({ url })
-
-//   setInterval(() => {
-//     autoUpdater.checkForUpdates()
-//   }, 60000)
-// }
-
-const createWindow = () => {
-  const win = new BrowserWindow({
+function createWindow () {
+  win = new BrowserWindow({
     width: 1280,
     height: 720,
     icon: __dirname + '/assets/images/logo.ico',
-    webPreferences: { devTools: false }
+    webPreferences: {
+      devTools: false,
+      preload: __dirname + '/js/preload.js',
+      nodeIntegration: true
+    }
   })
 
   win.loadFile('index.html')
@@ -34,28 +30,12 @@ app.whenReady().then(() => {
   })
 
   autoUpdater.checkForUpdates();
-  // curWindow.showMessage(`Checking for updates. Current version ${app.getVersion()}`);
 });
 
 /*New Update Available*/
 autoUpdater.on("update-available", (info) => {
-  // curWindow.showMessage(`Update available. Current version ${app.getVersion()}`);
   autoUpdater.downloadUpdate();
-  // curWindow.showMessage(pth);
 });
-
-// autoUpdater.on("update-not-available", (info) => {
-//   curWindow.showMessage(`No update available. Current version ${app.getVersion()}`);
-// });
-
-// /*Download Completion Message*/
-// autoUpdater.on("update-downloaded", (info) => {
-//   curWindow.showMessage(`Update downloaded. Current version ${app.getVersion()}`);
-// });
-
-// autoUpdater.on("error", (info) => {
-//   curWindow.showMessage(info);
-// });
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
@@ -70,24 +50,3 @@ app.on('browser-window-blur', function () {
   globalShortcut.unregister('CommandOrControl+R');
   globalShortcut.unregister('F5');
 });
-
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-  const dialogOpts = {
-    type: 'info',
-    buttons: ['Redémarrer', 'Plus tard'],
-    title: 'Mise à jour de QYourself',
-    message: process.platform === 'win32' ? releaseNotes : releaseName,
-    detail:
-      "Une nouvelle version de QYourself est disponible. Redémarrez l'application pour la mettre à jour."
-  }
-
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {
-    if (returnValue.response === 0) autoUpdater.quitAndInstall()
-  })
-})
-
-autoUpdater.on('error', (message) => {
-  console.error('Il y a eu un problème lors de la mise à jour')
-  console.error(message)
-})
